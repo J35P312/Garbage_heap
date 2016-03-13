6c93498d4c53857d9bb08efd2091ffa095699cc5
Jesper Eisfeldt <J35P312@users.noreply.github.com>
2016-03-13 15:22:45 +0100

    Update annotate_vcf_cadd.py

--- annotation/annotate_vcf_cadd.py
+++ annotation/annotate_vcf_cadd.py
@@ -11,8 +11,8 @@ def main(args):
         if line[0] == “#” and line[1] == “#”:
            print line.strip()
         elif line[0] == “#”:
-           print ##INFO=<ID=CADD,Number=1,Type=Float,Description=\"The phredd CADD score of the variant\">"
-           print ##INFO=<ID=FRQ,Number=1,Type=Float,Description=\"The frequency of the event in the database\">"
+           print "##INFO=<ID=CADD,Number=1,Type=Float,Description=\"The phredd CADD score of the variant\">"
+           print "##INFO=<ID=FRQ,Number=1,Type=Float,Description=\"The frequency of the event in the database\">"
            print line.strip()
         else:
            CADD=0
@@ -38,11 +38,11 @@ def main(args):
 	   #popfreq
         
 
-parser = argparse.ArgumentParser("""this scripts annotates a SNP using CADD and popfreq, the popfreq and CADD file must be tabix indexed and tabbix must be installed“””)
+parser = argparse.ArgumentParser("""this scripts annotates a SNP using CADD and popfreq, the popfreq and CADD file must be tabix indexed and tabbix must be installed""")
 parser.add_argument('--vcf',type=str,required=True,help="the path to the vcf file")
 parser.add_argument('--folder',type=str,help="used instead of vcf to annotate each vcf in a folder")
-parser.add_argument(‘-—cadd’,type=str,help=“The path to the CADD DB“)
-parser.add_argument('--popfreq’,type=str,help=“the “path to the popfreq DB”)
+parser.add_argument(‘-—cadd’,type=str,help="The path to the CADD DB")
+parser.add_argument('--popfreq’,type=str,help="the path to the popfreq DB")
 args, unknown = parser.parse_known_args()
 
 if args.vcf:
@@ -52,6 +52,6 @@ elif args.folder:
             for filename in fnmatch.filter(filenames, '*.vcf'):
                 bam_file=os.path.join(root, filename)
                 args.vcf=bam_file
-                main(args.vcf)
+                main(args)
 else:
     print("|>L3453 5|>3(1/=Y --vcf || --folder")
