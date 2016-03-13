1d0ea92e2dba4bfaae5bc85beb1556f66e0eda12
Jesper Eisfeldt <J35P312@users.noreply.github.com>
2016-03-13 15:26:10 +0100

    Update annotate_vcf_cadd.py
    fixed the mac characters

--- annotation/annotate_vcf_cadd.py
+++ annotation/annotate_vcf_cadd.py
@@ -8,41 +8,41 @@ def main(args):
 
     for line in open(args.vcf):
 
-        if line[0] == “#” and line[1] == “#”:
+        if line[0] == "#" and line[1] == "#":
            print line.strip()
-        elif line[0] == “#”:
+        elif line[0] == "#":
            print "##INFO=<ID=CADD,Number=1,Type=Float,Description=\"The phredd CADD score of the variant\">"
            print "##INFO=<ID=FRQ,Number=1,Type=Float,Description=\"The frequency of the event in the database\">"
            print line.strip()
         else:
            CADD=0
            FRQ=0
-           annotation=“;CADD={};FRQ={}”
-           content=line.split(“\t”)
+           annotation=";CADD={};FRQ={}"
+           content=line.split("\t")
 
            chromosome=content[0]
            position=int(content[1])
            end=position+1
            alt=content[4]
     	   #cadd annotation
-           command=[“tabix {} {}:{}-{}”.format(args.cadd,chromosome,position,end)]
+           command=["tabix {} {}:{}-{}".format(args.cadd,chromosome,position,end)]
            tmp=subprocess.check_output(command, shell = True);
-           output=tmp.split(“\n”)
+           output=tmp.split("\n")
            for entry in output:
-               db_content=entry.split(“\t”)
+               db_content=entry.split("\t")
                if db_content[4] == alt:
                   CADD=db_content[6]
                   break
            content[7] += annotation.format(CADD,FRQ)
-           print(“\t”.join(content))
+           print("\t".join(content))
 	   #popfreq
         
 
 parser = argparse.ArgumentParser("""this scripts annotates a SNP using CADD and popfreq, the popfreq and CADD file must be tabix indexed and tabbix must be installed""")
 parser.add_argument('--vcf',type=str,required=True,help="the path to the vcf file")
 parser.add_argument('--folder',type=str,help="used instead of vcf to annotate each vcf in a folder")
-parser.add_argument(‘-—cadd’,type=str,help="The path to the CADD DB")
-parser.add_argument('--popfreq’,type=str,help="the path to the popfreq DB")
+parser.add_argument('-—cadd',type=str,help="The path to the CADD DB")
+parser.add_argument('--popfreq',type=str,help="the path to the popfreq DB")
 args, unknown = parser.parse_known_args()
 
 if args.vcf:
