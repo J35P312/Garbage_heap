cb16c303a993e53798ccdc0d65b6427014c17768
J35P312 <jesper.eisfeldt@scilifelab.se>
2016-03-14 10:59:15 +0100

    modified:   annotation/combine_annovar_vcf.py

--- annotation/combine_annovar_vcf.py
+++ annotation/combine_annovar_vcf.py
@@ -7,25 +7,41 @@ parser.add_argument('--cadd',type=str,default=None,help="the path to the annovar
 parser.add_argument('--freq',type=str,default=None,help="the path to the annovar popfreqmax file(or any other frequency)")
 args, unknown = parser.parse_known_args()
 
+
+#reads the annotation of the annovar file
+def read_annovar_file(annovar_file):
+    annovar_annotation={}
+    for line in open(annovar_file):
+        content=line.split("\t")
+        if content[2] in annovar_file:
+            if content[6] in annovar_file[content[2]]:
+                annovar_annotation[content[2]][content[6]].append( [ content[3],content[1] ] )
+            else:
+                annovar_annotation[content[2]][content[6]]=[[ content[3],content[1] ]]
+        else:
+            annovar_annotation[content[2]] = { content[6]:[ [ content[3],content[1] ] ]}
+    return(annovar_annotation)
+
 def annotate_vcf(variant,annovar_file):
     annotation=""
     #check if teh variant is present in the annovar file, then add the annotation to the vcf, ele add "" to the vcf
     variant_info=variant.split("\t");
-    for line in open(annovar_file):
-        content=line.split("\t")
-        if content[2] == variant_info[0]:
-            if content[3] == variant_info[1]:
-                if content[6] == variant_info[4]:
-                    annotation=content[1]
-                    break
-    if (annotation == ""):
-        annotation="0"
-    return(annotation)
+    if variant_info[0] in annovar_file:
+        if variant_info[4] in annovar_file[variant_info[0]]:
+            for line in annovar_file[variant_info[0]][variant_info[4]]:
+                if content[0] == variant_info[1]:
+                    return(content[1])
+    return(0)
     
     
-CADD_HEADER="##INFO=<ID=CADD,Number=1,Type=Integer,Description=\"The CADD score of the variant\">"
-FREQ_HEADER="##INFO=<ID=FRQ,Number=1,Type=Integer,Description=\"The frequency of the event in the database\">"
+    
+CADD_HEADER="##INFO=<ID=CADD,Number=1,Type=Integer,Description=\"The CADD relative score for this alternative.\">"
+FREQ_HEADER="##INFO=<ID=1000GAF,Number=1,Type=Float,Description=\"Estimated allele frequency in the range (0,1) in the 1000G database.\">"
 
+if args.cadd:
+    CADD=read_annovar_file(args.cadd)
+if args.freq:
+    FRQ=read_annovar_file(args.freq)
 
 
 for line in open(args.vcf):
@@ -39,12 +55,12 @@ for line in open(args.vcf):
         print(line.strip())
     else:
         tag="";
-    
+        
         if args.cadd:
             tag +=";CADD="
-            tag += annotate_vcf(line,args.cadd)
+            tag += annotate_vcf(line,CADD)
         if args.freq:
-            tag += ";FRQ="
-            tag +=annotate_vcf(line,args.freq)
+            tag += ";1000GAF="
+            tag +=annotate_vcf(line,FRQ)
         content[7]+=tag
         print("\t".join(content))
