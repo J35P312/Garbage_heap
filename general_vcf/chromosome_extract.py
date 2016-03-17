7cf3303c930e6eedacb5a9c7a3758af7bac795b6
Jesper Eisfeldt <J35P312@users.noreply.github.com>
2016-03-17 14:10:45 +0100

    Update chromosome_extract.py

--- general_vcf/chromosome_extract.py
+++ general_vcf/chromosome_extract.py
@@ -2,7 +2,7 @@ import argparse
 
 parser = argparse.ArgumentParser("""this scripts prints every variant present on a chosen chromosome""")
 parser.add_argument('--vcf',type=str,required=True,help="the path to the vcf file")
-parser.add_argument('--chr',type=str,,required=True,help="the chromosome")
+parser.add_argument('--chr',type=str,required=True,help="the chromosome")
 args, unknown = parser.parse_known_args()
 
 for line in open(args.vcf):
