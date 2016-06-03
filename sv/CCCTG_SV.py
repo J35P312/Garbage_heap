12966005bc4c032d3fde775e30b8c9acfbec9ff5
J35P312 <jesper.eisfeldt@scilifelab.se>
2016-06-03 23:27:48 +0200

    new file:   sv/CCCTG_SV.py 	new file:   sv/readVCF.py

---
+++ sv/CCCTG_SV.py
@@ -0,0 +1,92 @@
+import sys
+import argparse
+from operator import itemgetter, attrgetter
+import xlwt
+import readVCF
+
+def EFF(effects):
+    snp_dictionary={}
+    for effect in effects:
+        if "HIGH" in effect or "MODERATE" in effect and "protein_coding" in effect:
+            variant=effect.split("(")[0]
+            if "[" in variant:
+                variant=variant.split("[")[0]
+            gene=effect.split("|")[5]
+            feature=effect.split("|")[3]
+            #for each snp, each unique effect of each gene shoudl only be reported once, ie not multiple intron variant GENEX for snp z
+            if not gene in snp_dictionary:
+                snp_dictionary[gene]={variant:feature}
+            else:
+                snp_dictionary[gene].update({variant:feature})
+        #if sequence_feature is not the only entry of a gene, 
+    return(snp_dictionary)
+    
+def ANN(effects):
+    snp_dictionary={}
+    for effect in effects:
+        if "HIGH" in effect or "MODERATE" in effect:
+            variant=effect.split("|")[1]
+            if "[" in variant:
+                variant=variant.split("[")[0]
+            gene=effect.split("|")[3]
+            feature=effect.split("|")[10]
+            #for each snp, each unique effect of each gene shoudl only be reported once, ie not multiple intron variant GENEX for snp z
+            if not gene in snp_dictionary:
+                snp_dictionary[gene]={variant:feature}
+            else:
+                snp_dictionary[gene].update({variant:feature})
+            #if sequence_feature is not the only entry of a gene, 
+    return(snp_dictionary)
+    
+parser = argparse.ArgumentParser("""turns a snpeff vcf into a csv file, output is printed to the stdout""")
+parser.add_argument('--vcf',type=str,required=True,help="the path to the vcf file")
+args, unknown = parser.parse_known_args()
+variant_list=[]
+
+output="\"{chrA}\",{chrB},\"{posA}\",\"{posB}\",\"{len}\",\"{var}\",\"{frequency}\",\"{genes}"
+for line in open(args.vcf):
+    if not "#" == line[0]:
+		chrA, posA, chrB, posB,event_type,INFO,format = readVCF.readVCFLine(line)
+        content=line.strip().split("\t")
+
+        #have a look in the snpeff field
+        eff=True
+        try:
+            SNPEFF=content[7].split("EFF=")[1];
+        except:
+                SNPEFF=content[7].split("ANN=")[1];
+                eff=False
+        effects=SNPEFF.split(",")
+        #generate one netry per gene
+        if eff:
+            snp_dictionary=EFF(effects)
+        else:
+            snp_dictionary=ANN(effects)
+            
+		genes=[]
+        for gene in snp_dictionary:
+			genes.append(gene)
+		length="infinite"
+		if chrA == chrB:
+			length= str(int(posB)-int(posA))
+        variant_list.append([chrA,chrB,posA,posB,length,event_type,INFO["FRQ"],"|".join(genes)])
+
+filename=args.vcf.replace(".vcf",".xls")
+
+wb =  xlwt.Workbook()
+ws0 = wb.add_sheet(filename.split("/")[-1],cell_overwrite_ok=True)
+i=0;
+header=["ChromosomeA","chromosomeB","PosA","PosB","Length","variant","frequency","Genes"]
+j=0
+for item in header:
+    ws0.write(i, j, item)
+    j+=1
+i=1
+for entry in sorted(variant_list): 
+    j=0;
+    for item in entry:
+        ws0.write(i, j, item)
+        j+=1
+    i+=1
+
+wb.save(filename)
