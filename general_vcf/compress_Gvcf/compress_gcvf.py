945d02c68457a8caff9632ce7280653a62a35ee1
J35P312 <jesper.eisfeldt@scilifelab.se>
2016-06-26 19:28:47 +0200

    modified:   compress_gcvf.py

--- general_vcf/compress_Gvcf/compress_gcvf.py
+++ general_vcf/compress_Gvcf/compress_gcvf.py
@@ -8,31 +8,27 @@ import subprocess
 #the script launches the script called delly, this script loads a module containing tabix and compresses it to gz files.
 
 def detect_bam_files(path):
-    bam_files={};
-    
-    for project in path:
-        path_to_sample = os.path.join(project.strip())
-        tmp_bam_files=[]
-        for root, dirnames, filenames in os.walk(path_to_sample):
-            for filename in fnmatch.filter(filenames, '*.genomic.vcf'):
-                file=(os.path.join(root, filename))
+    tmp_bam_files=[]
+    for root, dirnames, filenames in os.walk(path):
+#       print root
+        for filename in fnmatch.filter(filenames, '*.genomic.vcf'):
+            file=(os.path.join(root, filename))
                 
-                print file
-                process = "sbatch delly.sh {0}".format(file)
-                print process
-                p_handle = subprocess.Popen(process, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)    
-                p_out, p_err = p_handle.communicate()
+            print file
+            process = "sbatch delly.sh {0}".format(file)
+            print process
+            p_handle = subprocess.Popen(process, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)    
+            p_out, p_err = p_handle.communicate()
     
+    for root, dirnames, filenames in os.walk(path):
+        for filename in fnmatch.filter(filenames, '*.g.vcf'):
+            file=os.path.join(root, filename)
 
-        for root, dirnames, filenames in os.walk(path_to_sample):
-            for filename in fnmatch.filter(filenames, '*.g.vcf'):
-                file=os.path.join(root, filename)
-
-                print file
-                process = "sbatch delly.sh {0}".format(file)
-                print process
-                p_handle = subprocess.Popen(process, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)    
-                p_out, p_err = p_handle.communicate()
+            print file
+            process = "sbatch delly.sh {0}".format(file)
+            print process
+            p_handle = subprocess.Popen(process, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)    
+            p_out, p_err = p_handle.communicate()
                 
 
     return(tmp_bam_files)
